c0314fd84ddd4e7a9c67bc277d6463dd51b0cd8e
Jan Ivertowski <jan@ivertowski.de>
2011-03-15 15:53:31 +0100

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Product.delete_all
+
+Product.create(:title => 'PProgramming Ruby 1.9',
+	:description =>
+		%{<p>
+			Ruby is the fastest growing and most exciting dynamic language out
+			there. If you need to get working programs delivered fast, you should
+			add Ruby to  your toolbox.
+		</p>},
+	:image_url => '/images/ruby.jpg',
+	:price => 49.59)
