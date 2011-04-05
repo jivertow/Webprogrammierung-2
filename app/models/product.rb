3344367fb462613dad11e9c9971599588dcb29f9
Jan Ivertowski <jan@ivertowski.de>
2011-04-05 16:12:10 +0200

    added missing end tag in product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,10 +9,11 @@ default_scope :order => 'title'
 	before_destroy :ensure_not_referenced_by_any_line_item
 	
 	def ensure_not_referenced_by_any_line_item
-	if line_items.count.zero?
-		return true
-	else
-		errors[:base] << "Line Items present"
-		return false
+		if line_items.count.zero?
+			return true
+		else
+			errors[:base] << "Line Items present"
+			return false
+		end
 	end
 end
